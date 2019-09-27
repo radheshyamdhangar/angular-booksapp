182a25f512d0a1e43baa837f08e8c79c8aeb7792
radheshyamdhangar <50437447+radheshyamdhangar@users.noreply.github.com>
2019-09-27 09:12:54 +0530

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,24 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { BookmarkComponent } from './bookmark/bookmark.component';
+import { BookmarkListComponent } from './bookmark/bookmark-list/bookmark-list.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent,
+    BookmarkComponent,
+    BookmarkListComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    CommonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
